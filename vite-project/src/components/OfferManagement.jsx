a43d396c7d9cab8b6468f471dc97320c2f4dce68
ganeshkadam771 <ganeshkadam771@gmail.com>
2025-02-04 08:45:37 +0530

    Added search bar and create offer section skeleton

--- vite-project/src/components/OfferManagement.jsx
+++ vite-project/src/components/OfferManagement.jsx
@@ -3,6 +3,8 @@ import React from "react";
 export default function OfferManagement() {
   return (
     <div className="flex h-screen">
+
+        {/* Side bar  */}
       <aside className="w-64 bg-gradient-to-b from-purple-600 to-purple-800 p-4 text-white">
         <h2 className="text-xl font-bold mb-6">V-retail</h2>
         <nav>
@@ -15,6 +17,38 @@ export default function OfferManagement() {
           </ul>
         </nav>
       </aside>
+    {/* Main content  */}
+      <main>
+        <div>
+          <h1> Create and Manage Offers </h1>
+          <div></div>
+        </div>
+        {/* Search Bar  */}
+        <div>
+          <input type="text" placeholder="Search for offers..." className="" />
+          < d/>
+        </div>
+        <div>
+            {/* Create Offer Section  */}
+            <section>
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <label></label>
+                    <div>
+                        <Button/>
+                        <Button/>
+                        <Button/>
+                    </div>
+                    <input/>
+                    <input/>
+                    <textarea></textarea>
+                    <Button ></Button>
+                </div>
+            </section>
+        </div>
+      </main>
     </div>
   );
 }
