72b53fd75d989b3d2ceb05ac5948f7c33bb4bb98
ganeshkadam771 <ganeshkadam771@gmail.com>
2025-02-04 08:16:34 +0530

    Added basic skelaton for sidebar and fixed vite.cofig.file import issue

--- vite-project/src/components/OfferManagement.jsx
+++ vite-project/src/components/OfferManagement.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-export default function  OfferManagement(){
-
-    return (
-
-    );
+export default function OfferManagement() {
+  return (
+    <div >
+      <aside >
+        <h2 >V-retail</h2>
+        <nav>
+          <ul>
+            <li>Seats</li>
+            <li>Sales Members</li>
+            <li>Analytics</li>
+            <li>Chats & Tags</li>
+            <li >Custom</li>
+          </ul>
+        </nav>
+      </aside>
+    </div>
+  );
 }
